b85b2dcc69751d53533a612aec2e9d621786ce82
Yuraima <yurm04@gmail.com>
2020-01-30 02:31:57 -0500

    started over

---
+++ components/demo/AlertModal.js
@@ -0,0 +1,30 @@
+import React, { useRef, useEffect } from 'react'
+
+const AlertModal = ({ 
+  alertTitle='',
+  alertMessage='',
+  closeHanlder,
+  closeText 
+}) => {
+  // create an empty ref
+  const buttonRef = useRef(null)
+  
+  useEffect(() => {
+    // focuses the keyboard on the modal button when component renders
+    buttonRef.current.focus()
+  }, [])
+
+  return (
+    <div 
+      className="modal"
+      role="alertdialog"
+      aria-labeledby="alertTitle"
+      aria-describedby="alertMessage">
+      <h2 id="alertTitle">{alertTitle}</h2>
+      <p id="alertMessage">{alertMessage}</p>
+      <button
+        onClick={closeHanlder}
+        ref={buttonRef}>{closeText}</button>
+    </div>
+  )
+}
